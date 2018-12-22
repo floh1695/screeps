d1004575f1a492f7134dbf4e377ccc3488ac0af8
Charles Bayley <cbayley95@gmail.com>
2018-12-22 13:59:34 -0500

    Moved the old solution out

--- main.js
+++ main.js
@@ -1,37 +1,12 @@
 'use strict';
 
-const RoleApi = require('role');
-const { 
-  realizeRole,
-} = RoleApi.functions;
+//const solution1 = require('./solution-1');
 
 /** IO () */
-const loop = function () {
-  verifyCreepExists('harvester1');
-  haveCreepsWork();
-};
-
-/** String -> IO () */
-const verifyCreepExists = function (creepName) {
-  if (!Game.creeps[creepName]) {
-    Game.spawns['Spawn1']
-      .createCreep([ MOVE, WORK, CARRY ],
-        creepName,
-        { 
-          role: 'harvester',
-        }
-      );
-  }
-};
-
-/** IO () */
-const haveCreepsWork = function () {
-  Object.values(Game.creeps)
-    .map(realizeRole)
-    .forEach(roleCreep => roleCreep.run());
+const loop = () => {
+    
 };
 
 module.exports = {
   loop,
 };
-
---
+++ solution-1/index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const solution1 = require('./solution-1');
+
+const RoleApi = require('role');
+const { 
+  realizeRole,
+} = RoleApi.functions;
+
+/** IO () */
+const loop = function () {
+  verifyCreepExists('harvester1');
+  haveCreepsWork();
+};
+
+/** String -> IO () */
+const verifyCreepExists = function (creepName) {
+  if (!Game.creeps[creepName]) {
+    Game.spawns['Spawn1']
+      .createCreep([ MOVE, WORK, CARRY ],
+        creepName,
+        { 
+          role: 'harvester',
+        }
+      );
+  }
+};
+
+/** IO () */
+const haveCreepsWork = function () {
+  Object.values(Game.creeps)
+    .map(realizeRole)
+    .forEach(roleCreep => roleCreep.run());
+};
+
+module.exports = {
+  loop,
+};
+
